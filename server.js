e960cbf612a68224e563af5e3d09fdd7b9cc089d
Alex Fox <ajrfox@yahoo.co.uk>
2018-06-04 13:46:20 +0100

    modifying for heroku

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-
+const PORT = process.env.PORT || 8000;
 
 app.use(bodyParser.json());
 app.set('view engine', 'hbs');
-app.use('/static', express.static('static'));
+app.use(express.static(__dirname + '/static'));
 
 
 app.get('/',function(req,res){
       res.render('artledger');
 });
 
-app.listen(8080, function() {
+app.listen(PORT, function() {
   console.log('Listening on port 8080!');
 });
